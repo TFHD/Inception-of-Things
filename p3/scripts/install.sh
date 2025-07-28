6c50652806571a9febb1d704f533b43e54ca71be
Sacha <sacha.barthonet@gmail.com>
2025-07-28 19:19:39 +0200

    New start and install script, project is finished 💫

---
+++ p3/scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Docker installation 
+
+echo "[\e[31mDocker Installation]\e[0m" && sleep 1
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+#K3D installation
+
+echo "[\e[31mK3D Installation]\e[0m" && sleep 1
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+#kubectl installation
+
+echo "[\e[31mKubectl Installation]\e[0m" && sleep 1
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+#Argocd installation 
+
+VERSION=$(curl -s https://api.github.com/repos/argoproj/argo-cd/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -sSL -o argocd "https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64"
+chmod +x argocd
+sudo mv argocd /usr/local/bin/

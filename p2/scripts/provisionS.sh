2616987fed6a99984b4e74aa4c2e910d7ebd9e63
sabartho <59579542+TFHD@users.noreply.github.com>
2025-08-03 03:30:53 +0200

    add eth1 interface

--- p2/scripts/provisionS.sh
+++ p2/scripts/provisionS.sh
@@ -1,6 +1,8 @@
 curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up
+
 kubectl apply -f /vagrant/confs/app1-deployment.yaml
 kubectl apply -f /vagrant/confs/app2-deployment.yaml
 kubectl apply -f /vagrant/confs/app3-deployment.yaml
